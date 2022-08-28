92a978214f4fff903c503162559b0e02efd67f78
parmeet171 <97600602+parmeet171@users.noreply.github.com>
2022-08-28 08:38:39 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const responsive = {
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    520:{
+        items:2
+    },
+    960:{
+        items:3
+    },
+}
+
+
+
+$(document).ready(function () {
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse')
+    })
+
+    //owl crousel
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        responsive: responsive,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    });
+
+    $(".move-up span").click(function(){
+        $("html,body").animate({
+            scrollTop: 0 
+        }, 1000);
+    })
+
+    AOS.init();
+});
+
+
+
